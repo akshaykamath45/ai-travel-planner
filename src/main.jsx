c4fcd81aaf4d199eecd666e9ff22e20926e28f04
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-11-21 15:36:32 +0530

    feat: adding create-trip route

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CreateTrip from "./create-trip/index.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-  {},
+  {
+    path:"/create-trip",
+    element: <CreateTrip />
+  },
 ]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
