9664bde1967ede73df050246e6921abe4b73923d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-11-21 16:32:39 +0530

    style: creating hero section/landing page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="flex flex-col items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[50px] text-center mt-16">
+        <span className="text-[#17CF97]">Explore the World with Al:</span>{" "}
+        <br />
+        Tailored Routes Right at Your Fingertips!
+      </h1>
+      <p className="text-xl text-gray-500">
+        Crafting unique itineraries that suit to your interests and budget, I
+        will act as your personal travel planner and curator.
+      </p>
+      <Button>Get Started, It's Free</Button>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
