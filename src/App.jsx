b88cc51d2461c40092e2e028fa44ef9104a5b7a3
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-11-21 15:21:06 +0530

    chore: added shadcn and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h2>Welcome to AI Travel Planner</h2>
+      <Button>Sign In</Button>
     </>
   )
 }
