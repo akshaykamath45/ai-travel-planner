965f396ed9c07538e623c756174eb38e0f81aafe
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-11-21 15:42:24 +0530

    fix: minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import CreateTrip from "./create-trip/index.jsx";
+import Header from "./components/custom/Header";
 
 const router = createBrowserRouter([
   {
@@ -11,12 +12,13 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path:"/create-trip",
-    element: <CreateTrip />
+    path: "/create-trip",
+    element: <CreateTrip />,
   },
 ]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header />
     <RouterProvider router={router} />
   </StrictMode>
 );
